a1b04f4be49d101c74ade202dbaa73fbde044f61
zulfikar fajri <fajrizulfikar85@gmail.com>
2020-02-22 22:05:37 +0700

    delete unused export

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import LoadingAnimation from "./component/LoadingAnimation"
 
-export interface Props {
+interface Props {
   isLoading: boolean,
   children: JSX.Element
 }
